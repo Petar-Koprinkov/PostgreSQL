1327f1f33533799846e7d1af98e18aee80b2f795
Petar Koprinkov <134071867+Petar-Koprinkov@users.noreply.github.com>
2024-05-18 21:25:57 +0300

    Add files via upload

---
+++ basic-crud-lab/delete-from-table.sql
@@ -0,0 +1,11 @@
+DELETE 
+FROM
+	employees
+WHERE
+	department_id in (2, 1);
+
+SELECT 
+	*
+FROM 
+	employees
+ORDER BY id;
---
+++ basic-crud-lab/select-Employees-by-filtering.sql
@@ -0,0 +1,9 @@
+SELECT 
+	id,
+	CONCAT_WS(' ', first_name, 
+	last_name ) AS "Full Name",
+	job_title,
+	salary
+FROM employees
+WHERE salary > 1000
+ORDER BY id;
