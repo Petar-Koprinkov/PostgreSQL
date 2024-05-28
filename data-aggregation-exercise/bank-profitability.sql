33357346166114748b5fe6b30e384f10da02a5c5
Petar Koprinkov <134071867+Petar-Koprinkov@users.noreply.github.com>
2024-05-28 10:35:56 +0300

    Add files via upload

---
+++ data-aggregation-exercise/bank-profitability.sql
@@ -0,0 +1,16 @@
+SELECT	
+	deposit_group,
+	is_deposit_expired,
+	TRUNC(AVG(deposit_interest))
+FROM
+	wizard_deposits
+WHERE
+	deposit_start_date > '1985-01-01'
+GROUP BY
+	deposit_group,
+	is_deposit_expired 
+ORDER BY 
+	deposit_group DESC,
+	is_deposit_expired;
+
+
